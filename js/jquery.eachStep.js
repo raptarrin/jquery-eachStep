dc00533f576f7636b55fcbf757e387cefb44f0ba
Derek Anderson <derek@mediaupstream.com>
2011-08-06 02:16:59 -0500

    Fixed issue with requestAnimFrame, thanks to @jackrugile

--- js/jquery.eachStep.js
+++ js/jquery.eachStep.js
@@ -42,36 +42,61 @@ THE SOFTWARE.
 /**
  * better setTimeout thanks to http://blog.joelambert.co.uk/2011/06/01/a-better-settimeoutsetinterval/
  */
+// requestAnimationFrame() shim by Paul Irish
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function() {
+	return  window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+/**
+ * Behaves the same as setTimeout except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+
 window.requestTimeout = function(fn, delay) {
-    if( !window.requestAnimationFrame       && 
-        !window.webkitRequestAnimationFrame && 
-        !window.mozRequestAnimationFrame    && 
-        !window.oRequestAnimationFrame      && 
-        !window.msRequestAnimationFrame)
-            return window.setTimeout(fn, delay);
+	if( !window.requestAnimationFrame      	&& 
+		!window.webkitRequestAnimationFrame && 
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      && 
+		!window.msRequestAnimationFrame)
+			return window.setTimeout(fn, delay);
 
-    var start = new Date().getTime(),
-        handle = new Object();
+	var start = new Date().getTime(),
+		handle = new Object();
 
-    function loop(){
-        var current = new Date().getTime(),
-        delta = current - start;
+	function loop(){
+		var current = new Date().getTime(),
+			delta = current - start;
 
-        delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
-    };
+		delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+	};
 
-    handle.value = requestAnimFrame(loop);
-    return handle;
+	handle.value = requestAnimFrame(loop);
+	return handle;
 };
 
+/**
+ * Behaves the same as clearInterval except uses cancelRequestAnimationFrame() where possible for better performance
+ * @param {int|object} fn The callback function
+ */
 window.clearRequestTimeout = function(handle) {
     window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
-    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value)   :
+    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value)	:
     window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
-    window.oCancelRequestAnimationFrame ? window.oCancelRequestAnimationFrame(handle.value) :
+    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
     window.msCancelRequestAnimationFrame ? msCancelRequestAnimationFrame(handle.value) :
     clearTimeout(handle);
 };
+
+
+
 	
 
 // eachStep for Collections
